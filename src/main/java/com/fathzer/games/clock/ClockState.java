c9d8891ea564a58ca729e4a510ceb5513db33c1f
Fathzer <jm-dev@astesana.net>
2023-04-25 14:32:18 +0200

    One more step

--- src/main/java/com/fathzer/games/clock/ClockState.java
+++ src/main/java/com/fathzer/games/clock/ClockState.java
@@ -14,26 +14,42 @@ class ClockState {
 	
 	ClockState(ClockSettings settings) {
 		this.settings = settings;
+		this.countingSince = new AtomicLong(-1);
 		this.remainingMs = new AtomicLong(settings.getInitialTime()*1000);
+	}
+	
+	protected long getCurrentTime() {
+		return System.currentTimeMillis();
 	}
 	
 	synchronized void move() {
 		
 	}
 	
-	synchronized void pause() {
-		
+	synchronized long pause() {
+		if (!isPaused()) {
+			// If clock is already paused, do nothing
+			final long elapsed = getCurrentTime()-countingSince.get();
+			remainingMs.set(remainingMs.get()-elapsed);
+		}
+		return remainingMs.get();
 	}
 	
-	synchronized void start() {
-		
+	synchronized long start() {
+		if (isPaused()) {
+			// If clock is alreadyStarted, do nothing
+			countingSince.set(getCurrentTime()); 
+		}
+		return remainingMs.get();
 	}
 	
 	synchronized long getRemainingTime() {
-		return 0; //TODO
+		final long start = countingSince.get();
+		final long remaining = remainingMs.get();
+		return start < 0 ? remaining : remaining + start - getCurrentTime();
 	}
 	
 	synchronized boolean isPaused() {
-		return false; //TODO
+		return countingSince.get()<0;
 	}
 }
