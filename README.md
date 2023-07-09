ba4ef346a46687f0f67b73df6b418f1f5977985e
Fathzer <jm-dev@astesana.net>
2023-07-09 12:08:41 +0200

    Separates status from move list and removes it from Rules

--- README.md
+++ README.md
@@ -7,9 +7,8 @@
 A core library to help implement two players games
 
 ## TODO
-- Adds links under maven and license badges
-* Probably it should be better to force evaluation functions to return short instead of integer and makes Evaluator responsible for the computation of mat evaluations
-* Implement transposition table to increase search depth, in AI and PerfT
-* Write tests and Documentation ;-)
-* Test with Sonar
-* Publish artifact on Maven central
+- Probably it should be better to force evaluation functions to return short instead of integer and makes Evaluator responsible for the computation of mat evaluations
+- Implement transposition table to increase search depth, in AI and PerfT
+- Write tests and Documentation ;-)
+- Test with Sonar
+- Publish artifact on Maven central
--- src/main/java/com/fathzer/games/Rules.java
+++ src/main/java/com/fathzer/games/Rules.java
@@ -1,18 +1,13 @@
 package com.fathzer.games;
 
 /** The game's rules.
+ * //TODO Change comment
  * <br>Typically, it is able to create the representation of a new game (for example, a chessboard at the beginning of the game)
  * and compute the {@link GameState} of any valid representation of that game (for example, for any position of pieces on a chessboard).
  * <br>Here <i>valid</i> means, for instance for chess game, with kings on the chessBoard.  
  * @param <T> The class that represents the game. 
- * @param <M> The class that represents a move.
  */
-public interface Rules<T, M> {
+public interface Rules<T> {
 	/** Creates a new game.*/
 	T newGame();
-	/** Gets the state of a game.
-	 * @param board The representation of the game.
-	 * @return the game's state.
-	 */
-	GameState<M> getState(T board);
 }
