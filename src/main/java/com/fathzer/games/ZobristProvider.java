b8e4db868396957013faa6dc65c3decd23ec014f
Fathzer <jm-dev@astesana.net>
2023-07-10 18:43:41 +0200

    Merge with majorRefactoring

--- src/main/java/com/fathzer/games/ZobristProvider.java
+++ src/main/java/com/fathzer/games/ZobristProvider.java
@@ -3,6 +3,7 @@ package com.fathzer.games;
 /** A class able to compute a <a href="https://en.wikipedia.org/wiki/Zobrist_hashing">Zobrist key</a> for the current game state.
  * <br>Typically, a chess {@link MoveGenerator} which supports Zobrist hashing will implement this interface.
  */
+@FunctionalInterface
 public interface ZobristProvider {
 	/** Gets the hash key of the current game state.
 	 * @return a long
