be5d6276b3a6d4d9862089bdd83631ab2c176917
Fathzer <jm-dev@astesana.net>
2024-02-08 19:44:15 +0100

    Adds java doc on ZeroSumEvaluator

--- src/main/java/com/fathzer/games/ai/evaluation/ZeroSumEvaluator.java
+++ src/main/java/com/fathzer/games/ai/evaluation/ZeroSumEvaluator.java
@@ -9,8 +9,13 @@ import com.fathzer.games.MoveGenerator;
  * @param <B> The type of the chess move generator
  */
 public interface ZeroSumEvaluator<M, B extends MoveGenerator<M>> extends Evaluator<M, B> {
+	/** Evaluates the board from the white player point of view.
+	 * @param board The board to evaluate
+	 * @return an integer.
+	 */
 	int evaluateAsWhite(B board);
 	
+	@Override
 	default int evaluate(B board) {
 		final int points = evaluateAsWhite(board);
 		return board.isWhiteToMove() ? points : - points;
