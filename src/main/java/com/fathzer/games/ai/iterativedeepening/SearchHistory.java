d4a3fcdd20f80a94115d416c83cff534d1d0bba1
Astesana <jm-dev@astesana.net>
2025-02-28 17:12:08 +0100

    Removes getBest & getBestMove from SearchHistory

--- src/main/java/com/fathzer/games/ai/iterativedeepening/SearchHistory.java
+++ src/main/java/com/fathzer/games/ai/iterativedeepening/SearchHistory.java
@@ -3,16 +3,25 @@ package com.fathzer.games.ai.iterativedeepening;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fathzer.games.ai.SearchParameters;
 import com.fathzer.games.ai.SearchResult;
 import com.fathzer.games.ai.evaluation.EvaluatedMove;
 import com.fathzer.games.ai.moveselector.MoveSelector;
 
+/** A history of search results in an iterative deepening search.
+ * @see IterativeDeepeningEngine
+ */
 public class SearchHistory<M> {
 	private final int size;
 	private final int accuracy;
 	private final List<List<EvaluatedMove<M>>> results;
 	private final List<Integer> depths;
 	
+	/** Constructor
+	 * <br>The parameters should be the one used in {@link SearchParameters#SearchParameters(int, int)} constructor.
+	 * @param size How many best moves are requested to have an exact value (Integer.MAX_VALUE to have all moves).
+	 * @param accuracy How many moves are requested to have an exact value (Integer.MAX_VALUE to have all moves).
+	 */
 	public SearchHistory(int size, int accuracy) {
 		this.size = size;
 		this.accuracy = accuracy;
@@ -20,6 +29,10 @@ public class SearchHistory<M> {
 		this.depths = new ArrayList<>();
 	}
 	
+	/** Adds a new search result at a specified depth.
+	 * @param result The search result
+	 * @param depth The depth at which the result was obtained
+	 */
 	public void add(List<EvaluatedMove<M>> result, int depth) {
 		if (!isEmpty() && depth<depths.get(depths.size()-1)) {
 			throw new IllegalArgumentException();
@@ -27,47 +40,72 @@ public class SearchHistory<M> {
 		results.add(result);
 		depths.add(depth);
 	}
-	
-	public int getDepth(int index) {
-		return depths.get(index);
-	}
 
-	public List<EvaluatedMove<M>> getList(int index) {
-		return results.get(index);
-	}
-
-	public List<EvaluatedMove<M>> getBestMoves(int index) {
-		return SearchResult.getBestMoves(results.get(index), size, accuracy);
-	}
-
-	public List<EvaluatedMove<M>> getList() {
-		return results.isEmpty() ? null : results.get(results.size()-1);
-	}
-	public List<EvaluatedMove<M>> getBestMoves() {
-		return results.isEmpty() ? null : SearchResult.getBestMoves(results.get(results.size()-1), size, accuracy);
-	}
-	
-	public EvaluatedMove<M> getBestMove(MoveSelector<M, SearchHistory<M>> selector) {
-		return selector.select(this, getBestMoves()).get(0);
-	}
-
-	public EvaluatedMove<M> getBestMove() {
-		return getBestMoves().get(0);
-	}
-	
-	public M getBest() {
-		return isEmpty() ? null : getBestMoves().get(0).getMove();
-	}
-	
+	/** Returns the number of results added with {@link #add(List, int)} method
+	 * @return a positive integer
+	 */
 	public int length() {
 		return results.size();
 	}
 	
+	/** Checks whether this history is empty
+	 * @return true if this history is empty
+	 */
 	public boolean isEmpty() {
 		return results.isEmpty();
 	}
-	
+
+	/** Returns the depth of a result added with {@link #add(List, int)} method
+	 * @param index The index of the result
+	 * @return a positive integer
+	 */
+	public int getDepth(int index) {
+		return depths.get(index);
+	}
+
+	/** Returns the list of moves of a result added with {@link #add(List, int)} method
+	 * @param index The index of the result
+	 * @return a list of moves
+	 */
+	public List<EvaluatedMove<M>> getList(int index) {
+		return results.get(index);
+	}
+
+	/** Returns the best moves of a result added with {@link #add(List, int)} method.
+	 * @param index The index of the result
+	 * @return a list of moves restricted to the size and accuracy of this history
+	 */
+	public List<EvaluatedMove<M>> getBestMoves(int index) {
+		return SearchResult.getBestMoves(results.get(index), size, accuracy);
+	}
+
+	/** Gets the depth of the last list of moves added to this history
+	 * @return a positive integer (0 if no results has been added to this history)
+	 */
 	public int getDepth() {
-		return results.isEmpty() ? 0 : depths.get(results.size()-1);
+		return isEmpty() ? 0 : depths.get(results.size()-1);
+	}
+
+	/** Gets the last list of moves added to this history
+	 * @return a list of moves, or null if this history is empty
+	 */
+	public List<EvaluatedMove<M>> getList() {
+		return isEmpty() ? null : results.get(results.size()-1);
+	}
+	
+	/** Gets the last list of best moves added to this history
+	 * @return a list of moves (restricted to the size and accuracy of this history), or null if this history is empty
+	 */
+	public List<EvaluatedMove<M>> getBestMoves() {
+		return isEmpty() ? null : SearchResult.getBestMoves(results.get(results.size()-1), size, accuracy);
+	}
+
+	/** Gets the best move according to the given selector
+	 * @param selector The selector to use
+	 * @return the best move
+	 */
+	public EvaluatedMove<M> getBestMove(MoveSelector<M, SearchHistory<M>> selector) {
+		//FIXME getBestMoves can return null
+		return selector.select(this, getBestMoves()).get(0);
 	}
 }
--- src/test/java/com/fathzer/games/ai/terativedeepening/IterativeDeepeningEngineTest.java
+++ src/test/java/com/fathzer/games/ai/terativedeepening/IterativeDeepeningEngineTest.java
@@ -41,11 +41,11 @@ class IterativeDeepeningEngineTest {
 		final DeepeningPolicy deepeningPolicy = new DeepeningPolicy(4);
 		IterativeDeepeningEngine<Move, ChessLibMoveGenerator> engine = new IterativeDeepeningEngine<>(deepeningPolicy, new TT(16, SizeUnit.MB), BasicEvaluator::new);
 		ChessLibMoveGenerator mg = new ChessLibMoveGenerator("r1bq1rk1/3n1ppp/p3p3/2bpP3/Np1B1P1P/7R/PPPQ2P1/2KR1B2 b - - 1 14", BasicMoveComparator::new);
-		assertEquals(new Move(C5, D4),engine.getBestMoves(mg).getBest());
+		assertEquals(new Move(C5, D4), getBest(engine.getBestMoves(mg)));
 		final Move illegalMove = new Move(D3, D4);
 		assertTrue(engine.getBestMoves(mg, Collections.singletonList(illegalMove)).isEmpty());
 		final Move legalMove = new Move(A6, A5);
-		assertEquals(legalMove, engine.getBestMoves(mg, Arrays.asList(illegalMove, legalMove)).getBest());
+		assertEquals(legalMove, getBest(engine.getBestMoves(mg, Arrays.asList(illegalMove, legalMove))));
 	}
 	
 	@Test
@@ -53,7 +53,7 @@ class IterativeDeepeningEngineTest {
 		final DeepeningPolicy deepeningPolicy = new DeepeningPolicy(4);
 		IterativeDeepeningEngine<Move, ChessLibMoveGenerator> engine = new IterativeDeepeningEngine<>(deepeningPolicy, new TT(16, SizeUnit.MB), BasicEvaluator::new);
 		ChessLibMoveGenerator mg = new ChessLibMoveGenerator("r1bq1rk1/3n1ppQ/p3p3/2bpP3/Np1B1P1P/3B3R/PPP3P1/2KR4 b - - 1 14", BasicMoveComparator::new);
-		assertNull(engine.getBestMoves(mg).getBest());
+		assertNull(getBest(engine.getBestMoves(mg)));
 	}
 	
 	@Test
@@ -242,5 +242,10 @@ class IterativeDeepeningEngineTest {
 		public Status getEndGameStatus() {
 			return null;
 		}
+	}
+	
+	private Move getBest(SearchHistory<Move> history) {
+		List<EvaluatedMove<Move>> moves = history.getBestMoves();
+		return moves==null || moves.isEmpty() ? null : moves.get(0).getMove(); 
 	}
 }
